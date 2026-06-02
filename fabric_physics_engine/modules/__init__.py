ee1c78c92960acc114399ed292b04bf6a2faec8b
ceaicum-cmd <ceaicum@gmail.com>
2026-06-02 22:01:19 +0300

    Add modules package init

---
+++ fabric_physics_engine/modules/__init__.py
@@ -0,0 +1,17 @@
+"""Modules package."""
+
+__all__ = [
+    "core_fabric_physics",
+    "gravity_system",
+    "tension_system",
+    "compression_system",
+    "fold_generation",
+    "material_control",
+    "seam_construction",
+    "layering_interaction",
+    "micro_wrinkles",
+    "motion_inertia",
+    "anti_artifact",
+    "cinematic_realism",
+    "body_responsive_adaptation",
+]
